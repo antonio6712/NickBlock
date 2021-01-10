a6180835a813fa2b69df5f761a719d102944e4e9
antonio6712 <antonio.flores67@hotmail.com>
2021-01-10 16:27:52 -0600

    1

---
+++ README.md
@@ -0,0 +1,1 @@
+"# NickBlock" 
---
+++ styles/styles.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+
+ScrollReveal().reveal('.showcase' , { delay: 600 });
+ScrollReveal().reveal('.news-cars' , { delay: 600 });
+ScrollReveal().reveal('.cars-banner-one' , { delay: 600 });
+ScrollReveal().reveal('.social' , { delay: 600 });
