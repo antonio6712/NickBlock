c9636bb249c0715bceae94216c97a2aa48b27b03
antonio6712 <antonio.flores67@hotmail.com>
2021-02-06 15:05:02 -0600

    sintaxis

--- php/contacto.php
+++ php/contacto.php
@@ -2,11 +2,11 @@
 //Destinatario
 $destinatario = 'antonio.flores67@hotmail.com';
 
-$fullname = $_POST['fullname']
-$email = $_POST['email']
-$phone = $_POST['phone']
-$affair = $_POST['affair']
-$message = $_POST['message']
+$fullname = $_POST['fullname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$affair = $_POST['affair'];
+$message = $_POST['message'];
 
 $header = 'NickBlock';
 $fullMesage = $message . "\nAtentamente" . $fullname . "\nTelefono" . $phone . "\nCorreo" . $email ;
