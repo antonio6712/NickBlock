35aac5489c08d744045e95eccd86b9d2da305d28
antonio6712 <antonio.flores67@hotmail.com>
2021-02-06 14:59:16 -0600

    correo

---
+++ php/contacto.php
@@ -0,0 +1,17 @@
+<?php
+//Destinatario
+$destinatario = 'antonio.flores67@hotmail.com';
+
+$fullname = $_POST['fullname']
+$email = $_POST['email']
+$phone = $_POST['phone']
+$affair = $_POST['affair']
+$message = $_POST['message']
+
+$header = 'NickBlock';
+$fullMesage = $message . "\nAtentamente" . $fullname . "\nTelefono" . $phone . "\nCorreo" . $email ;
+
+mail($destinatario, $affair, $fullMesage, $header );
+
+
+?>
