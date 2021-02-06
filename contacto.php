2b5bf5becdd76b7694b4e645812d6f5c520cce7b
antonio6712 <antonio.flores67@hotmail.com>
2021-02-06 15:48:18 -0600

    modificacion contacto

---
+++ contacto.php
@@ -0,0 +1,27 @@
+<?php
+
+$fullname = $_POST['fullname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+$header = 'From: ' . $email . "\r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . "\r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain \r\n";
+
+$message = "Este mensaje fue enviado por: " . $fullname . "\r\n";
+$message .= "Su e-mail es: " . $email . "\r\n";
+$message .= "Telefono de contacto: " . $phone . "\r\n";
+$message .= "Mensaje: " . $_POST['message'] . "\r\n";
+$message .= "Enviando el: " . date('d/m/Y', time());
+
+$para = 'antonio.flores67@hotmail.com';
+$asunto = $_POST['affair'];
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+
+
+?>
